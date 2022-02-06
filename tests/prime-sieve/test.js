60dc7ea0776ca754753003c09bc45c6e372a36da
Johan Wiltink <johan@wilt.ink>
2022-02-06 21:42:00 +0100

    fix testing

--- tests/prime-sieve/test.js
+++ tests/prime-sieve/test.js
@@ -15,10 +15,10 @@ const take = n => xs => n ? [ head(xs), ...take(n-1)(tail(xs)) ] : [] ;
 
 it("fixed tests: primes", function() {
   this.timeout(12e3);
-  assert.equal( head(primes), 2 );
-  assert.equal( head(tail(primes)), 3 );
-  assert.equal( head(tail(tail(primes))), 5 );
-  assert.equal( head(tail(tail(tail(primes)))), 7 );
+  assert.equal( toInt(head(primes)), 2 );
+  assert.equal( toInt(head(tail(primes))), 3 );
+  assert.equal( toInt(head(tail(tail(primes)))), 5 );
+  assert.equal( toInt(head(tail(tail(tail(primes))))), 7 );
   assert.deepEqual( take(100)(primes).map(toInt), [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71
                                                   ,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149
                                                   ,151,157,163,167,173,179,181,191,193,197,199,211,223,227
