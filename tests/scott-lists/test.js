34915a5319e70b83c704d5524716605585c6634b
Johan Wiltink <johan@wilt.ink>
2022-02-09 02:57:50 +0100

    update scott-list

--- tests/scott-lists/test.js
+++ tests/scott-lists/test.js
@@ -16,6 +16,8 @@ const {foldl,reverse} = solution;
 
 const fromList = foldl ( z => x => [...z,x] ) ([]) ;
 
+const refReplicate = length => x => Array.from( { length }, () => x ) ;
+
 describe("Scott Lists",function(){
   it("example tests",()=>{
     assert.deepEqual( fromList( nil ), [] );
@@ -29,7 +31,7 @@ describe("Scott Lists",function(){
     const rnd = (m,n=0) => Math.random() * (n-m) + m | 0 ;
     for ( let i=1; i<=100; i++ ) {
       const m = rnd(i), n = rnd(i);
-      assert.deepEqual( fromList( replicate (fromInt(m)) (String(n)) ), [], `after ${ i } tests` );
+      assert.deepEqual( fromList( replicate (fromInt(m)) (String(n)) ), refReplicate(m)(String(n)), `after ${ i } tests` );
     }
   });
 });
