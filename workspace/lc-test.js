460110c9e120ce3a1690a90d0f6b2ffa1e365aff
kazk <kazk.dev@gmail.com>
2022-02-17 12:28:45 -0800

    Export `chai.config`

--- workspace/lc-test.js
+++ workspace/lc-test.js
@@ -1,8 +1,6 @@
 import { readFileSync } from "fs";
 
-import { assert, config as chaiConfig } from "chai";
-chaiConfig.truncateThreshold = 0;
-export { assert };
+export { assert, config } from "chai";
 export * as LC from "@codewars/lambda-calculus";
 
 const read = (path) => readFileSync(new URL(path, import.meta.url), {encoding: "utf8"});
